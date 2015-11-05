b5a299eaf314d0d83f1c4c680ec27de4d2725524
Asbjorn Kjaer <akjaer@riotgames.com>
2015-11-04 17:30:56 -0800

    Fixing third party plugin

---
+++ third_party/third_party_plugin/plugins/third_party.py
@@ -0,0 +1,20 @@
+__author__ = 'bunjiboys'
+
+import logging
+
+log = logging.getLogger(__name__)
+
+class ThirdPartyPlugin(object):
+    def run(self, *args, **kwargs):
+        log.info('run method executed. self: {self}, args: {args}, kwargs: {kwargs}'.format(
+            self=str(self),
+            args=", ".join(args),
+            kwargs=", ".join( [ "{0}={1}".format(k, v) for k,v in kwargs.items() ] )
+        ))
+
+    @staticmethod
+    def run_static(*args, **kwargs):
+        log.info('run_static method executed. args: {args}, kwargs: {kwargs}'.format(
+            args=", ".join(args),
+            kwargs=", ".join( [ "{0}={1}".format(k, v) for k,v in kwargs.items() ] )
+        ))
